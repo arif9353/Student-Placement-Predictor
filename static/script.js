51265fc093211d23917cda4448146ba9c4b4e560
arif9353 <ma782318@gmail.com>
2024-01-06 15:24:41 +0530

    Updated the changes to deploy on Heroku

--- static/script.js
+++ static/script.js
@@ -194,7 +194,7 @@ function showQuestion(){
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question.replace(/\n/g, "<br>");
 
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
